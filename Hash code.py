162a3dcead0712cdff03be4a49d0b6d84e90bcf6
berkanefesunal <66440491+berkanefesunal@users.noreply.github.com>
2020-08-17 15:03:35 +0200

    Update Hash code.py

--- Hash code.py
+++ Hash code.py
@@ -1,13 +1,15 @@
 #For implementation of Hash tables we use Dictionaries.
-studentID={'Name':'John','studentnumb':1234,'Name2':"Alice",'studentnumb':3423}
+studentID={'Name':'John','studentnumb':1234,'Name2':"Alice",'studentnumb2':3423}
 name=studentID['Name']
 print(name)
-studentID["studentnumb"]
+studentnumb=studentID["studentnumb2"]
+print(studentnumb)
 
 #let's update an element
 studentID['Name']='Computer Science Student'
 studentID['Name2']='PHD computer science student.'
 
 #delete
-
 del studentID["Computer Science Student."]
+
+# WE USE DICTIONARIES AS AN EXAMPLE OF HASH TABLES.
