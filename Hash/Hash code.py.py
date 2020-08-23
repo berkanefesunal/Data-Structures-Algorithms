05e1687e21c375d8a32ef6adc659c2da0696cad8
berkanefesunal <66440491+berkanefesunal@users.noreply.github.com>
2020-08-23 11:46:12 +0200

    Add files via upload

---
+++ Hash/Hash code.py.py
@@ -0,0 +1,13 @@
+#For implementation of Hash tables we use Dictionaries.
+studentID={'Name':'John','studentnumb':1234,'Name2':"Alice",'studentnumb':3423}
+name=studentID['Name']
+print(name)
+studentID["studentnumb"]
+
+#let's update an element
+studentID['Name']='Computer Science Student'
+studentID['Name2']='PHD computer science student.'
+
+#delete
+
+del studentID["Computer Science Student."]
